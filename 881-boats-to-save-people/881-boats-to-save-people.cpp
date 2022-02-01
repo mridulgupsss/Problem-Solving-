299ce8ebe45fe135d78dbb85c32e6feafa679ac2
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-01 14:42:46 +0530

    Time: 166 ms (9.44%), Space: 44.8 MB (5.07%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,19 +1,33 @@
 class Solution {
-public:
+public: // Using bucket sort it can be done in O(N) time:
     int numRescueBoats(vector<int>& people, int limit) {
-        int ans =0;
-        sort(people.begin(), people.end());
-        int i=0, j=people.size()-1;
+        /* nlogn is occuring due to sorting and we know that every person can go on boat so array of people cannot contain element greater than the limit , so we can use bucket sort to reduce sorting complexity and then using the pointer approach on the count array; 
+        */
+        
+      int ans =0;
+      vector<int> count (limit+1, 0);
+        for(auto val: people ){
+            count[val]++;
+        }
+         // now 2 pointer on count array;
+        int i=0, j=limit;
         while(i<=j){
-            if(people[i]+people[j]<=limit){
-                ans++; i++; j--;
-            }
+            if(count[i]==0) i++;
+            else if(count[j]==0) j--;
             else{
-                j--;
-                ans++;
+                if(i==j && count[i]==1){
+                    ans++; break;
+                }
+                else if(i+j<=limit){
+                    count[i]--; count[j]--;
+                    ans++;
+                    cout<<ans;
+                }
+                else {
+                    count[j]--; ans++;
+                }
             }
         }
         return ans;
-        
     }
 };
