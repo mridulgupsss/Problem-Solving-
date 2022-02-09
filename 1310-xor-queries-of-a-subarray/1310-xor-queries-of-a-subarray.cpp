da4569e524197cd7e816313c5199ad2cd9fc9ff5
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-09 12:21:03 +0530

    Time: 93 ms (63.91%), Space: 38.4 MB (36.69%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -3,34 +3,32 @@ public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n= arr.size();
         vector<int> left(n);
-        vector<int> right(n);
+
         int xorx=0;
-        int xory=0;
+        
         
         for(int i=0; i<n; i++){
             xorx^=arr[i];
             left[i]=xorx;
             
-            xory^=arr[n-i-1];
-            right[n-i-1]=xory;
+
         }  
         
         
         vector<int> ans ;
         for(int i=0; i<queries.size(); i++){
-            int total_xor = right[0];
+        
             int a=queries[i][0];
             int b=queries[i][1];
             
             if(a!=0){
-               total_xor^=left[a-1];      
+               ans.push_back(left[a-1]^left[b]);      
             }
             
-            if(b!=n-1){
-                total_xor^=right[b+1];
+            else{
+                ans.push_back(left[b]);
             }
             
-            ans.push_back(total_xor);
         }
         
         return ans;
