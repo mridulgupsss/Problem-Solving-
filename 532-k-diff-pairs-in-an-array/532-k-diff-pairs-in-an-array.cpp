8d74c9743fe97524a1acc9b33b4ce043b996a30a
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-10 01:12:17 +0530

    Time: 17 ms (77.09%), Space: 13.2 MB (58.91%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    bool binSearch(int target, int lo, int hi, vector<int> &v){
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            if(v[mid]==target) return true;
-            else if(v[mid]>target) hi=mid-1;
-                else lo=mid+1;
-        }
-        
-        return false;
-    }
     int findPairs(vector<int>& arr, int k) {
-        sort(arr.begin(), arr.end()); int ans=0;
-        int i=0; int n=arr.size();
-        while(i<n){
-            int val=arr[i];
-            int target = k+val;
-            
-           if(binSearch(target, i+1, n-1, arr)==true) ans++; 
-            
-            while(i<n && arr[i]==val) i++;
+        unordered_map<int, int> mp; int ans=0;
+        for(auto val: arr){
+            mp[val]++;
         }
         
+        for(auto it: mp){
+            int val = it.first;
+            int target =val-k; // consider val>other value
+            if((k>0 && mp.find(target)!=mp.end()) || (k==0 && mp[val]>1)){
+                ans++;
+            }
+        }
         return ans;
     }
 };
