c068852d74b92b789674cf54cfc369ffbd34a903
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-14 00:33:16 +0530

    Time: 13 ms (73.96%), Space: 10.4 MB (28.12%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,9 +1,13 @@
 class Solution {
-public:
+public: 
+    //  nice article: with 2 approaches ...2nd one same with better code 
+    
+ /* link:   https://leetcode.com/problems/car-pooling/discuss/1670052/C%2B%2B-or-2-Approaches-or-priority-queue-or-with-intuition-or-Easy-to-understand-explanation-or-STL */
+    
     bool carPooling(vector<vector<int>>& trips, int capacity) {
        int arr[1001]={0};
         for(auto it : trips){
-            arr[it[1]]+=it[0];
+            arr[it[1]]+=it[0];  // be carefull 
             arr[it[2]]+=-it[0];
         }
         int sum=0;
