d5562199fd35b0d9f505ca4ca3a035c77e8ae010
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-18 13:21:57 +0530

    Time: 184 ms (97.26%), Space: 84.3 MB (26.32%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,33 +1,25 @@
 class StockSpanner {
 public:
-    vector<pair<int,int>> v; // price , ans
-    
+    stack<pair<int, int> >st; // price , idx
+    int idx;
     StockSpanner() {
-        
+        idx=-1;
     }
     
     int next(int price) {
-        int idx = v.size();
-        if(idx==0){
-            v.push_back({price, 1});
+               idx++;
+
+        while(!st.empty() && st.top().first<=price){
+            st.pop();
         }
+        int prev;
+        if(st.empty()) prev=-1;
         else{
-            if(v[idx-1].first>price){
-                v.push_back({price, 1});
-            }
-            else{
-                int ans = 1+v[idx-1].second;
-                int newIdx=idx-v[idx-1].second-1;
-                while(newIdx>=0 && v[newIdx].first<=price){
-                    ans+=v[newIdx].second;
-                    newIdx=newIdx-v[newIdx].second;
-                }
-                
-                v.push_back({price, ans});
-            }
+            prev =  st.top().second;
         }
         
-        return v[v.size()-1].second;
+        st.push({price,idx});
+        return idx-prev;
     }
 };
 
