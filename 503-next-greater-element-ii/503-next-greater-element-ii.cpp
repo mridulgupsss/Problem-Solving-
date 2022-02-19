c45584c04ad2104809164e053081cfa21ce4c3da
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-19 19:47:08 +0530

    Time: 41 ms (63.34%), Space: 23.7 MB (89.31%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -4,24 +4,29 @@ public:
        int n = arr.size();
         vector<int> ans(n);
         stack<int> st;
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.top()<=arr[i]){
+        st.push(0);
+        for(int i=1; i<n; i++){
+            while(!st.empty() && arr[st.top()]<arr[i]){
+                ans[st.top()] = arr[i];
                 st.pop();
             }
-            st.push(arr[i]);
+            st.push(i);
             
         }
         
         
-        for(int i=n-1; i>=0; i--){
-            while(!st.empty() && st.top()<=arr[i]){
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[st.top()]<arr[i]){
+                ans[st.top()]=arr[i];
                 st.pop();
             }
-            if(st.empty()) ans[i]=-1;
-            else ans[i]=st.top();
             
-            
-            st.push(arr[i]);           
+           
+        }
+        
+        while(!st.empty()){
+            ans[st.top()]=-1;
+            st.pop();
         }
         
         return ans;
