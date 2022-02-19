ce1996d840d94075e3713ead482d64d6948e88bb
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-19 14:11:47 +0530

    Time: 255 ms (25.03%), Space: 77.2 MB (12.40%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -6,8 +6,8 @@ public:
         int n = arr.size();
     int ans =0;
         st.push(0);
-        for(int i=0; i<n; i++){
-            while(!st.empty() && arr[i]<arr[st.top()]){
+        for(int i=0; i<=n; i++){
+            while(!st.empty() && (i==n || arr[i]<arr[st.top()])){
                 int idx=st.top();
                 st.pop();
                 if(st.empty()){
@@ -23,23 +23,7 @@ public:
             
             st.push(i);
         }
-        
-        while(!st.empty()){
-            int idx=st.top(); st.pop();
-                if(st.empty()){
-                    int potential =(arr[idx]*1LL*(n));
-                    ans=max(ans, potential);
-                }
-                else{
-                    int potential = arr[idx]*1LL*(n-st.top()-1);
-                    ans = max(ans, potential);
-                }
-            
-        }
-        
-        
-        
-        
+    
         return ans;
     }
 };
