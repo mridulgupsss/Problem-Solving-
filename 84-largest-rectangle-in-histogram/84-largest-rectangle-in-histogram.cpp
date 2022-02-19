f0e7eccffd58d869285aa02e15d4c24fef423867
Mridul Gupta <83277458+mridulgupsss@users.noreply.github.com>
2022-02-19 14:09:21 +0530

    Time: 136 ms (79.50%), Space: 77.2 MB (12.40%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -2,35 +2,43 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& arr) {
         // next smaller ele to left and right 
-        stack<int> st1, st2;
+        stack<int> st;
         int n = arr.size();
-         st2.push(n-1);
-        vector<int> v (n);
-        v[n-1]=n;
-        for(int i=n-2; i>=0; i--){
-            while(!st2.empty() && arr[st2.top()]>=arr[i]){
-                st2.pop();
+    int ans =0;
+        st.push(0);
+        for(int i=0; i<n; i++){
+            while(!st.empty() && arr[i]<arr[st.top()]){
+                int idx=st.top();
+                st.pop();
+                if(st.empty()){
+                    int potential =(arr[idx]*1LL*(i));
+                    ans=max(ans, potential);
+                }
+                else{
+                    int potential = arr[idx]*1LL*(i-st.top()-1);
+                    ans = max(ans, potential);
+                }
+            
             }
-            if(st2.empty()) v[i]=n;
-            else v[i]=st2.top();
-            st2.push(i);
+            
+            st.push(i);
         }
         
-        int ans =arr[0];
-        for(int i=0; i<n; i++){
-
-            while(!st1.empty() && arr[st1.top()]>=arr[i]){
-                st1.pop();
-            }
-            int left;
-            if(st1.empty()) left=-1;
-            else left= st1.top();
-            st1.push(i);
-         
-            int area = arr[i]*1LL*(v[i]-left-1);
-            ans = max(ans, area);
-
+        while(!st.empty()){
+            int idx=st.top(); st.pop();
+                if(st.empty()){
+                    int potential =(arr[idx]*1LL*(n));
+                    ans=max(ans, potential);
+                }
+                else{
+                    int potential = arr[idx]*1LL*(n-st.top()-1);
+                    ans = max(ans, potential);
+                }
+            
         }
+        
+        
+        
         
         return ans;
     }
